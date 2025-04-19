8582f60f0a9d39564197ab1378ac1d0bd407f0da
Руслан <yruslan2013@gmail.com>
2025-04-19 16:58:51 +0300

    добавили кнопку равно

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Calculator:
         for x in range(1,5):
             self.buttons_frame.rowconfigure(x, weight = 1)
             self.buttons_frame.columnconfigure(x, weight=1)
+        self.create_equals_buttons()
 
     def run(self):
         self.window.mainloop()
@@ -53,13 +54,13 @@ class Calculator:
 
     def create_digit_buttons(self):
         for digit, grid_value in self.digits.items():
-            button = tk.Button(self.buttons_frame, text = str(digit), bg = "white", fg = "black", font = ("Arial", 24, "bold"), borderwidth = 0)
+            button = tk.Button(self.buttons_frame, text = str(digit), bg = "white", fg = "black", font = ("Arial", 24, "bold"), borderwidth = 0, command = lambda x = digit: self.add_to_expression(x))
             button.grid(row = grid_value[0],column = grid_value[1], sticky = tk.NSEW)
 
     def create_operator_buttons(self):
         i = 0
         for operator, symbol in self.operations.items():
-            button = tk.Button(self.buttons_frame, text=symbol, bg="white", fg="black", font=("Arial", 24, "bold"),borderwidth=0)
+            button = tk.Button(self.buttons_frame, text=symbol, bg="white", fg="black", font=("Arial", 24, "bold"),borderwidth=0, command = lambda x = operator: self.append_operator(x))
             button.grid(row = i, column = 4, sticky = tk.NSEW)
             i+=1
 
@@ -69,6 +70,33 @@ class Calculator:
     def update_label(self):
         self.label.config(text=self.current_expression[:11])
 
+    def add_to_expression(self, value):
+        self.current_expression += str(value)
+        self.update_label()
+
+    def append_operator(self, operator):
+        self.current_expression += operator
+        self.total_expression += self.current_expression
+        self.current_expression =""
+        self.update_total_label()
+        self.update_label()
+
+    def create_equals_buttons(self):
+        button = tk.Button(self.buttons_frame, text="=", bg="white", fg="black", font=("Arial", 24, "bold"),
+                           borderwidth=0, command=self.evaluate)
+        button.grid(row=4, column=3, columnspan=2, sticky=tk.NSEW)
+
+    def evaluate(self):
+        self.total_expression += self.current_expression
+        self.update_total_label()
+        try:
+            self.current_expression = str(eval(self.total_expression))
+
+            self.total_expression = ""
+        except Exception as e:
+            self.current_expression = "Error"
+        finally:
+            self.update_label()
 
 if __name__ == "__main__":
     calc = Calculator()
